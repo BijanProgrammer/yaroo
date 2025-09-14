0ed5a1efa713135aea9de589555a33b45543add4
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-14 06:20:54 +0330

    [ADD] react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Link, Route, Routes } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const header = (
+    <header>
+      <Link to="/">Home</Link>
+      {` `}
+      <Link to="/about">About</Link>
+    </header>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>YAROO</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route
+        index
+        element={
+          <div>
+            {header}
+            <h1>Home</h1>
+          </div>
+        }
+      />
+      <Route
+        path="about"
+        element={
+          <div>
+            {header}
+            <h1>About</h1>
+          </div>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
